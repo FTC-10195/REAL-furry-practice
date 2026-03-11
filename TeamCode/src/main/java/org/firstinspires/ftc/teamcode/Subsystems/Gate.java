3dce41b88d683981e659f72a347d1fe9d3cd518a
Lakshya-Square <lakshyakhowala2012@gmail.com>
2026-03-10 21:48:27 -0400

    Update Gate.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Gate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Gate.java
@@ -1,4 +1,42 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
 public class Gate {
+    public enum States {
+        OPEN,
+        CLOSE
+    }
+
+    States currentState = States.CLOSE;
+    Servo gate;
+    public double openPosition = 0.7;
+    public double closePosition = 0.5;
+    public void initiate(HardwareMap hardwareMap) {
+        gate = hardwareMap.servo.get("gate");
+
+    }
+
+    public void setCurrentState(States newState) {
+        currentState = newState;
+    }
+
+    public States getCurrentState() {
+        return currentState;
+    }
+
+    public void shoot() {
+        setCurrentState(States.OPEN);
+    }
+
+    public void update() {
+        if (currentState == States.OPEN){
+            gate.setPosition(openPosition);
+        }else{
+            gate.setPosition(closePosition);
+        }
+    }
+
+
 }
